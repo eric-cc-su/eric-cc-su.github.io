797ce0739c855d68fa4b8103fcac3c75eca7f0b0
eric-cc-su <eric.cc.su@gmail.com>
2015-05-28 20:39:39 -0400

    Small changes

--- javascripts/index.js
+++ javascripts/index.js
@@ -41,11 +41,11 @@ var main = function() {
         if ($(window).scrollTop() != 0) {
             nvb.stop().animate( {backgroundColor:'#FFF'}, {duration:300});
             nvb.css("box-shadow", "0 0 10px rgba(0,0,0,0.5)");
-            $('.nav > li > a').stop().animate({color:'#60695C'}, {duration:300});
+            $('.nav > li > a').css({color:'#60695C'}, {duration:300});
             fork.stop().animate( {height: '60px', borderRadius: "0", right: "0"}, {duration:300});
             fork.css("box-shadow","none");
             if (isFirefox) {
-                $('#forkme_banner > p').css( {margin: "10px 0"}, {duration:300});
+                $('#forkme_banner > p').css('margin', "10px 0");
             }
             else {
                 $('#forkme_banner > p').animate( {margin: "10px 0"}, {duration:300});
@@ -54,11 +54,11 @@ var main = function() {
         else {
             nvb.stop().animate( {backgroundColor:"transparent"}, {duration:300});
             nvb.css("box-shadow", "none");
-            $('.nav > li > a').stop().animate({color:'#FFF'}, {duration:300});
+            $('.nav > li > a').css({color:''}, {duration:300});
             fork.stop().animate( {height: '44px', borderRadius: "0 0 2px 2px", right: "10px"}, {duration:300});
             fork.css("box-shadow", "0 0 10px rgba(0,0,0,0.5)");
             if (isFirefox) {
-                $('#forkme_banner > p').css( {margin: "0"}, {duration:300});
+                $('#forkme_banner > p').css('margin', "0");
             }
             else {
                 $('#forkme_banner > p').animate( {margin: "0"}, {duration:300});
