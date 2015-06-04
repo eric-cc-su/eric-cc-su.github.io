b505536ea2c37b95b884b45e3ee5871eb737a400
Eric Su <eric.cc.su@gmail.com>
2015-06-04 15:20:25 -0400

    Layout & Page changes

--- javascripts/infoslide.js
+++ javascripts/infoslide.js
@@ -3,45 +3,39 @@
  */
 var screenheight = window.innerHeight;   //measures the height of the user's screen
 
-//calculate the heights of page divs and positioning of text elements
+//calculate the heights of page divs
 var calculate_win = function() {
-    var foot = document.getElementById("footer_wrap");
-    var lpg = document.getElementsByClassName("page-landing")[0];
-    foot.style.top = lpg.clientHeight.toString() + "px";
-
-    if (window.innerHeight < 600) { //only resize elements if screen is of appropriate height (reduces f**k-ups)
-        return;
-    }
-
-    var landing = document.getElementById("homecenter");
-    var mn = document.getElementById("maincontain");
     screenheight = window.innerHeight;
 
-    foot.style.top = screenheight.toString() + "px";
-    landing.style.marginTop = ((0.25*screenheight)-60) + "px";
+    var landing = $(".page-landing:first");
+    var content = $("#homecenter");
+    var difference = landing.outerHeight() - content.outerHeight();
 
-    var reqheight = landing.scrollHeight + 60 + ((0.25*screenheight)-60);
-    lpg.style.height = reqheight.toString() + "px";
-    mn.style.height = reqheight.toString() + "px";
-    if (screenheight > reqheight) {
-        lpg.style.height =  screenheight.toString() + "px";
-        mn.style.height =  screenheight.toString() + "px";
+    if (screenheight >= landing.outerHeight(true)){
+        landing.css("height", screenheight.toString() + "px");
+        content.css({
+            "margin-top": (difference-60).toString() + "px",
+            "margin-bottom": difference.toString() + "px"
+        });
+
+    }
+    else {
+        landing.css("height","inherit");
+        content.css({
+            "margin-top":"",
+            "margin-bottom":""
+        });
+
     }
 };
 
 var main = function() {
     screenheight = window.innerHeight;
-    try {
-        calculate_win(); //initialize sizes
-    }
-    catch(err) {}
+    calculate_win();
 
     //recalculate on window resize
     window.onresize = function() {
-        try {
-            calculate_win();
-        }
-        catch(err) {}
+        calculate_win();
     };
 };
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -4,32 +4,39 @@
  */
 
 var main = function() {
+    var nvb = $('#navb');
+    var nvb_orig_height = Number(nvb.css('height').replace("px",""));
     window.onscroll = function() {
-        var nvb = $('#navb');
-        if ($(window).scrollTop() != 0) {
-            nvb.css({
-                "background-color": "#FFF",
-                "box-shadow": "0 0 10px rgba(0,0,0,0.5)"
-            });
-            $('.navtext').css('color','#60695C');
-            $('.icon-bar').css('background-color','#60695C');
-        }
-        else {
-            var nvb_height = Number(nvb.css('height').replace("px",""));
-            if (nvb_height <= 61) {
+        if (document.getElementById('navb').className != "navbar navbar-static-top"){
+            if ($(window).scrollTop() != 0) {
                 nvb.css({
-                    "background-color": "",
-                    "box-shadow": "none"
+                    "background-color": "#FFF",
+                    "box-shadow": "0 0 10px rgba(0,0,0,0.5)"
                 });
-                $('.navtext').css('color','');
-                $('.icon-bar').css('background-color','');
+                $('.navtext').css('color','#60695C');
+                $('.icon-bar').css('background-color','#60695C');
+            }
+            else {
+                var nvb_height = Number(nvb.css('height').replace("px",""));
+                if (nvb_height <= nvb_orig_height) {
+                    nvb.css({
+                        "background-color": "",
+                        "box-shadow": "none"
+                    });
+                    $('.navtext').css('color','');
+                    $('.icon-bar').css('background-color','');
+                }
             }
         }
     };
 
     $('button[class="navbar-toggle collapsed"]').on('click', function() {
         var navb = $('#navb');
+        var nav_eric = $('a[title="Home"]');
         if ($('#navco').css('display') == 'none') {
+            if (nav_eric.hasClass('home')) {
+                nav_eric.show();
+            }
             navb.css('background-color','#FFF');
             navb.css("box-shadow", "0 0 10px rgba(0,0,0,0.5)");
             $('.navtext').css('color','#60695C');
@@ -37,6 +44,9 @@ var main = function() {
         }
         else {
             if ($(window).scrollTop() == 0) {
+                if (nav_eric.hasClass('home')) {
+                    nav_eric.hide();
+                }
                 navb.stop().animate({backgroundColor:"transparent"}, {duration:300});
                 navb.css("box-shadow", "none");
                 $('.navtext').css('color','');
