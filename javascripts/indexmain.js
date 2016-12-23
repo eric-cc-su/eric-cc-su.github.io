9f47b4046bf460d1d17825bce14d1e72c5ff0535
Eric <eric.cc.su@gmail.com>
2016-12-23 14:59:57 -0500

    Updated resume, added Contact page, modified pages

--- javascripts/indexmain.js
+++ javascripts/indexmain.js
@@ -24,20 +24,20 @@ var main = function() {
         }
     });
 
-    $('a[href="#work"').click(function() {
-        $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-work")});
-        console.log("work");
-    });
-
-    $('a[href="#skills"').click(function() {
-        $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-skills")});
-        console.log("skills");
-    });
-
-    $('a[href="#projects"').click(function() {
-        $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-projects")});
-        console.log("projects");
-    });
+    // $('a[href="#work"').click(function() {
+    //     $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-work")});
+    //     console.log("work");
+    // });
+    //
+    // $('a[href="#skills"').click(function() {
+    //     $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-skills")});
+    //     console.log("skills");
+    // });
+    //
+    // $('a[href="#projects"').click(function() {
+    //     $("#home-top").animate({opacity: 0},{duration: 400, complete: transition("home-projects")});
+    //     console.log("projects");
+    // });
 };
 
 $(document).ready(main);
